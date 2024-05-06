4bd66523133d088e00a524ddf8a50f357180b684
mgdemel <mgdemel@gmail.com>
2024-05-06 18:05:29 +0300

    test changing styles of components

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,19 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
 import svgr from 'vite-plugin-svgr';
+import ViteRestart from 'vite-plugin-restart';
 import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ exclude: ['**/*.stories.tsx'] }), svgr()], //svgr({ include: '**/*.svg' })
+  plugins: [
+    react(),
+    dts({ exclude: ['**/*.stories.tsx'] }),
+    svgr(),
+    ViteRestart({
+      restart: ['lib/components/**'],
+    }),
+  ], //svgr({ include: '**/*.svg' })
   build: {
     lib: {
       entry: resolve(__dirname, 'lib/index.ts'),
